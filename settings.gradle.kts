825dbf3a9bccaa288de7e7b636074812b646e1d0
Alex Shepeliev <a.shepeliev@gmail.com>
2022-02-02 11:08:47 +0200

    Cleanup project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,2 @@
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:4.2.0")
-            }
-        }
-    }
-}
 rootProject.name = "webrtc-kmp"
-
+include(":webrtc-kmp")
