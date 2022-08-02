c50249757b12783cfcaa14d1bed2de45646e75a4
Alex Shepeliev <a.shepeliev@gmail.com>
2022-08-02 23:43:45 +0300

    ios: init app

---
+++ sample/app-ios/app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-ios
+//
+//  Created by Aleksandr Shepeliev on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
