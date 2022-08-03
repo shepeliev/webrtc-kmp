12d86b4ba9422cfec6e44d3e5febf5414ffd4a93
Alex Shepeliev <a.shepeliev@gmail.com>
2022-08-03 17:13:03 +0300

    ios: prepare view controllers

---
+++ sample/app-ios/app-ios/RoomViewController.swift
@@ -0,0 +1,61 @@
+//
+//  RoomViewController.swift
+//  app-ios
+//
+//  Created by Aleksandr Shepeliev on 03.08.2022.
+//
+
+import UIKit
+import WebRTC
+import shared
+
+class RoomViewController: UIViewController {
+
+    private let room: Room! = (UIApplication.shared.delegate as! AppDelegate).room
+    private var localVideo = RTCMTLVideoView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLocalVideo()
+        setupLocalVideoConstraints()
+
+        room.model.subscribe(observer: roomModelObserver)
+        room.openUserMedia()
+    }
+    
+    private func roomModelObserver(_ model: RoomModel) {
+        if let localStream = model.localStream {
+            localStream.videoTracks.first?.addRenderer(renderer: localVideo)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        room.hangup()
+        room.model.unsubscribe(observer: roomModelObserver)
+    }
+    
+    // MARK: - UI setup
+    private func setupLocalVideo() {
+        view.insertSubview(localVideo, at: 0)
+    }
+    
+    private func setupLocalVideoConstraints() {
+        localVideo.translatesAutoresizingMaskIntoConstraints = false
+        localVideo.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        localVideo.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        localVideo.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        localVideo.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sample/app-ios/app-ios/StartViewController.swift
+++ sample/app-ios/app-ios/StartViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hangupAction(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
