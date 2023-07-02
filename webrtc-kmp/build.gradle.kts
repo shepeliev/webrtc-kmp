b6ec92792afb6f8ac46c79d49fee655404ced206
Alex Shepeliev <a.shepeliev@gmail.com>
2023-07-02 14:19:41 +0300

    Update WebRTC revision M114

--- webrtc-kmp/build.gradle.kts
+++ webrtc-kmp/build.gradle.kts
@@ -1,5 +1,3 @@
-import de.undercouch.gradle.tasks.download.Download
-
 plugins {
     id("multiplatform-setup")
     id("publish-setup")
@@ -47,31 +45,8 @@ android {
 dependencies {
     commonMainImplementation(deps.kotlin.coroutines)
     androidMainImplementation(deps.androidx.coreKtx)
-    androidMainApi(fileTree("build/libs/android") { include("*.jar") })
+    androidMainApi(deps.webrtcSdk)
     androidTestImplementation(deps.androidx.test.core)
     androidTestImplementation(deps.androidx.test.runner)
     jsMainImplementation(npm("webrtc-adapter", "8.1.1"))
-}
-
-tasks.register<Download>("downloadAndroidWebRtc") {
-    src("https://github.com/jitsi/webrtc/releases/download/v$jitsiWebRtcVersion/android-webrtc.zip")
-    dest(buildDir.resolve("tmp/android-webrtc-$jitsiWebRtcVersion.zip"))
-    overwrite(false)
-}
-
-tasks.register<Copy>("unzipAndroidWebRtc") {
-    inputs.files(tasks.findByName("downloadAndroidWebRtc")!!.outputs.files)
-    from(zipTree(inputs.files.first()))
-    into(buildDir.resolve("libs/android"))
-}
-
-afterEvaluate {
-    tasks.named("preBuild") {
-        dependsOn("unzipAndroidWebRtc")
-    }
-
-    // downloads Android WebRTC lib on gradle sync in Android Studio
-    tasks.named("commonize") {
-        dependsOn("unzipAndroidWebRtc")
-    }
 }
