78608824ec93d7ca3d0e5aedff686fc01fe5fe73
Alex Shepeliev <a.shepeliev@gmail.com>
2023-07-04 15:11:38 +0300

    Upgrade Kotlin 1.8.22, AGP 8.0.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+    implementation("com.android.tools.build:gradle:8.0.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
     implementation("de.undercouch:gradle-download-task:5.1.0")
 }
