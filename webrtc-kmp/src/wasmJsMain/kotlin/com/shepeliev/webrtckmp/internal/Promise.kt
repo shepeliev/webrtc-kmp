4bc32aa3830757138119aba572478d659b320ab0
Alex Shepeliev <a.shepeliev@gmail.com>
2024-05-08 11:09:24 +0300

    Update TODO comment

--- webrtc-kmp/src/wasmJsMain/kotlin/com/shepeliev/webrtckmp/internal/Promise.kt
+++ webrtc-kmp/src/wasmJsMain/kotlin/com/shepeliev/webrtckmp/internal/Promise.kt
@@ -6,7 +6,8 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.js.Promise
 
-// TODO: replace it by kotlinx.coroutines.await after releasing https://github.com/Kotlin/kotlinx.coroutines/pull/4120
+// TODO: remove after fixing in kotlinx.coroutines
+// PR: https://github.com/Kotlin/kotlinx.coroutines/pull/4120
 @Suppress("UNCHECKED_CAST")
 suspend fun <T> Promise<JsAny?>.await(): T = suspendCancellableCoroutine { cont: CancellableContinuation<T> ->
     this@await.then(
