221a90e5c6dcb16200b39fd4007189212991e4c4
Alex Shepeliev <a.shepeliev@gmail.com>
2024-05-08 10:57:23 +0300

    Fix typo

--- webrtc-kmp/src/commonMain/kotlin/com/shepeliev/webrtckmp/RtcCertificatePem.kt
+++ webrtc-kmp/src/commonMain/kotlin/com/shepeliev/webrtckmp/RtcCertificatePem.kt
@@ -1,9 +1,9 @@
 package com.shepeliev.webrtckmp
 
 expect class RtcCertificatePem {
-    @Deprecated("Will be removes in order to comply with JS/WASM")
+    @Deprecated("Will be removed in order to comply with JS/WASM")
     val privateKey: String
-    @Deprecated("Will be removes in order to comply with JS/WASM")
+    @Deprecated("Will be removed in order to comply with JS/WASM")
     val certificate: String
 
     companion object {
