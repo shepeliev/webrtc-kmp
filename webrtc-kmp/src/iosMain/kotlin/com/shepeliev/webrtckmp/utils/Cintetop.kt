8a8c0979d3efdce6f49de77a4aaece62351fe369
Alex Shepeliev <a.shepeliev@gmail.com>
2024-11-28 15:26:36 +0200

    Fix video capturing fails on iOS 18.1.1

---
+++ webrtc-kmp/src/iosMain/kotlin/com/shepeliev/webrtckmp/utils/Cintetop.kt
@@ -0,0 +1,13 @@
+package com.shepeliev.webrtckmp.utils
+
+import kotlinx.cinterop.CStructVar
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+
+@OptIn(ExperimentalForeignApi::class)
+internal inline fun <reified T : CStructVar> CValue<T>.copyContent(): T {
+    lateinit var value: T
+    this.useContents { value = this }
+    return value
+}
