8ecad0d64c2babb39122adfc52928214009fe04f
Yash-Garg <me@yashgarg.dev>
2025-06-28 12:32:30 +0530

    feat: add example for pulling audio samples

--- sample/composeApp/src/androidMain/kotlin/com/shepeliev/webrtckmp/sample/MainActivity.kt
+++ sample/composeApp/src/androidMain/kotlin/com/shepeliev/webrtckmp/sample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.shepeliev.webrtckmp.sample
 
 import App
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -8,16 +10,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.shepeliev.webrtckmp.WebRtc
+import org.webrtc.ApplicationContextProvider
 import org.webrtc.Loggable
 import org.webrtc.Logging
+import org.webrtc.audio.JavaAudioDeviceModule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val useHardwareAudioProcessing = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+        val audioModule =
+            JavaAudioDeviceModule.builder(ApplicationContextProvider.getApplicationContext())
+                .setUseHardwareAcousticEchoCanceler(useHardwareAudioProcessing)
+                .setUseHardwareNoiseSuppressor(useHardwareAudioProcessing)
+                .setSamplesReadyCallback { samples ->
+                    // Here you can process the audio buffer as needed
+                    println("onWebRtcAudioRecordSamplesReady: $samples")
+                }
+                .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+                .createAudioDeviceModule()
+
         val initializationOptionsBuilder = WebRtc.createInitializationOptionsBuilder()
             .setInjectableLogger(WebRtcLogger, Logging.Severity.LS_ERROR)
-        val peerConnectionFactoryBuilder = WebRtc.createPeerConnectionFactoryBuilder(initializationOptionsBuilder)
+        val peerConnectionFactoryBuilder =
+            WebRtc.createPeerConnectionFactoryBuilder(initializationOptionsBuilder)
+                .setAudioDeviceModule(audioModule)
+
         WebRtc.configure(peerConnectionFactoryBuilder = peerConnectionFactoryBuilder)
 
         setContent {
--- sample/composeApp/src/iosMain/kotlin/MainViewController.kt
+++ sample/composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,9 +1,17 @@
+import WebRTC.RTCAudioBuffer
+import WebRTC.RTCAudioCustomProcessingDelegateProtocol
 import WebRTC.RTCAudioSession
 import WebRTC.RTCAudioSessionConfiguration
+import WebRTC.RTCDefaultAudioProcessingModule
+import WebRTC.RTCDefaultVideoDecoderFactory
+import WebRTC.RTCDefaultVideoEncoderFactory
+import WebRTC.RTCPeerConnectionFactory
+import WebRTC.RTCVideoEncoderFactorySimulcast
 import WebRTC.setConfiguration
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.ComposeUIViewController
 import co.touchlab.kermit.Logger
+import com.shepeliev.webrtckmp.WebRtc
 import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.ObjCObjectVar
@@ -12,6 +20,8 @@ import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.value
 import platform.Foundation.NSError
+import platform.darwin.NSObject
+import platform.posix.size_t
 
 @Suppress("unused", "FunctionName")
 @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
@@ -32,5 +42,36 @@ fun MainViewController() = ComposeUIViewController {
         }
     }
 
+    val encoderFactory = RTCDefaultVideoEncoderFactory()
+    val peerConnectionFactory =
+        RTCPeerConnectionFactory(
+            bypassVoiceProcessing = false,
+            RTCVideoEncoderFactorySimulcast(encoderFactory, encoderFactory),
+            RTCDefaultVideoDecoderFactory(),
+            audioProcessingModule = RTCDefaultAudioProcessingModule(
+                config = null,
+                capturePostProcessingDelegate = AudioProcessingDelegate(),
+                renderPreProcessingDelegate = null
+            )
+        )
+
+    WebRtc.configure(rtcPeerConnectionFactory = peerConnectionFactory)
+
     App()
+}
+
+@OptIn(ExperimentalForeignApi::class)
+internal class AudioProcessingDelegate : NSObject(), RTCAudioCustomProcessingDelegateProtocol {
+    override fun audioProcessingInitializeWithSampleRate(sampleRateHz: size_t, channels: size_t) {
+        println("Audio processing initialized with sample rate: $sampleRateHz Hz, channels: $channels")
+    }
+
+    override fun audioProcessingProcess(audioBuffer: RTCAudioBuffer) {
+        println("Audio processing in progress with buffer: $audioBuffer")
+        // Here you can process the audio buffer as needed
+    }
+
+    override fun audioProcessingRelease() {
+        println("Audio processing released")
+    }
 }
